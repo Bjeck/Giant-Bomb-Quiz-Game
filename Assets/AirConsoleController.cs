7eef0d90cc92694f3827a5504650b2605d8304af
BJARKESPC\bjark <mail@bjarke.it>
2018-02-04 21:24:39 +0100

    you can now choose an answer with your PHONE??!??! only the first two tho :P

---
+++ Assets/AirConsoleController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NDream.AirConsole;
+using Newtonsoft.Json.Linq;
+
+public class AirConsoleController : MonoBehaviour {
+
+    public QuizManager quiz;
+
+
+    void Start()
+    {
+        AirConsole.instance.onMessage += OnMessage;
+    }
+
+    void OnMessage(int from, JToken data)
+    {
+        print("got thing "+data.ToString());
+        AirConsole.instance.Message(from, "Full of pixels!");
+
+
+        TranslateInputToGame(data);
+
+    }
+
+
+    void TranslateInputToGame(JToken data)
+    {
+        PickClass pc = data.ToObject<PickClass>();
+        print(pc.gameNr);
+        quiz.TestAnswer(quiz.uiGames[pc.gameNr - 1]);
+    }
+
+
+}
+
+public class PickClass
+{
+    public int gameNr;
+}
